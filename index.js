8a69fa22feedf68c74584a42fe49ce3da0bdcc7d
uchechi <isuuchechi@gmail.com>
2023-11-30 11:49:16 +0100

    color generator

--- index.js
+++ index.js
@@ -33,14 +33,39 @@ function ResetBtn(){
 
 function RGB() {
     const colorText = colorBox.style.backgroundColor;
-    const temInput =document.createElement("input");
-    document.body.appendChild(temInput);
-    temInput.value = colorText;
-    temInput.select();
+    const tempInput =document.createElement("input");
+    document.body.appendChild(tempInput);
+    tempInput.value = colorText;
+    tempInput.select();
     document.execCommand("copy");
-    document.body.removeChild(temInput);
+    document.body.removeChild(tempInput);
     alert("color copied to clipboard:" + colorText);
     
+    };
+    function copyHex() {
+      const colorText = colorBox.style.backgroundColor;
+      const hexcode = rgbToHex(colorText);
+      const tempInput = document.createElement("input");
+      document.body.appendChild(tempInput);
+      tempInput.value = hexcode;
+      tempInput.select();
+      document.execCommand("copy");
+      document.body.removeChild(tempInput);
+      alert("color copied to clipboard: " + hexcode);
+    
+    };
+    function rgbToHex(rgbColor) {
+        const regex = /rgb\((\d+), (\d+), (\d+)\)/;
+        const matches = regex.exec(rgbColor);
+        const r = parseInt(matches[1]);
+        const g = parseInt (matches[2]);
+        const b = parseInt (matches[3]);
+        return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+    
+    };
+    function componentToHex(c){
+        const hex = c.toString(16);
+        return hex. length == 1 ? "0" + hex : hex;
     };
 
 
